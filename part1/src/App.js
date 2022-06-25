cf1eec5c3792f41a784d81fc5a8a644397e5692b
ppwqdxlte <rexinwangmin@foxmail.com>
2022-06-25 15:33:09 +0800

    复杂状态：数组[]状态保存所有状态的变化

--- part1/src/App.js
+++ part1/src/App.js
@@ -36,23 +36,24 @@ const Button = ({onClick, text}) => (
 )
 
 const App = () => {
-    const [clicks,setClicks] = useState({
-        left:0, right:0
-    })
-    const handleLeftClick = () => setClicks({
-        ...clicks,
-        left: clicks.left + 1
-    })
-    const handleRightClick = () => setClicks({
-        ...clicks,
-        right: clicks.right + 1
-    })
+    const [left, setLeft] = useState(0)
+    const [right,setRight] = useState(0)
+    const [allClicks,setAll] = useState([])
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        setLeft(left + 1)
+    }
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setRight(right + 1)
+    }
     return (
         <>
-            { clicks.left }
-            <Button onClick={ handleLeftClick } text={'left'} />
-            { clicks.right }
-            <Button onClick={ handleRightClick } text={'right'} />
+            {left}
+            <Button onClick={handleLeftClick} text={'left'} />
+            {right}
+            <Button onClick={handleRightClick} text={'right'} />
+            <p>{allClicks.join(' ')}</p>
         </>
     )
 }
