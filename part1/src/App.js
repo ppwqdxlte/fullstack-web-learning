b83360fdd297a40e88300604c2484b16f12ba820
ppwqdxlte <rexinwangmin@foxmail.com>
2022-06-25 22:39:25 +0800

    处理器工厂：【返回函数的函数】，不同参数DIY不同处理函数

--- part1/src/App.js
+++ part1/src/App.js
@@ -51,24 +51,15 @@ const History = ({allClicks}) => {
 }
 
 const App = () => {
-    const [left, setLeft] = useState(0)
-    const [right,setRight] = useState(0)
-    const [allClicks,setAll] = useState([])
-    const handleLeftClick = () => {
-        setAll(allClicks.concat('L'))
-        setLeft(left + 1)
-    }
-    const handleRightClick = () => {
-        setAll(allClicks.concat('R'))
-        setRight(right + 1)
-    }
+    const [value,setValue] = useState('打招呼')
+    const hello = (props) =>
+        () => setValue('Hello,'+props)
     return (
         <>
-            {left}
-            <Button onClick={handleLeftClick} text={'left'} />
-            {right}
-            <Button onClick={handleRightClick} text={'right'} />
-            <History allClicks={allClicks} />
+            {value}
+           <Button onClick={hello('World')} text={'点点看'} />
+           <Button onClick={hello('React')} text={'点点看'} />
+           <Button onClick={hello('LaoWang')} text={'点点看'} />
         </>
     )
 }
