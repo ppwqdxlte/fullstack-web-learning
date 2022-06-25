a9431f2c5edf8e5bf3f983f7ecdcaa5ddc4c43a2
ppwqdxlte <rexinwangmin@foxmail.com>
2022-06-25 13:36:48 +0800

    函数入参结构，采用更紧凑的钩子函数返回形式

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,58 +1,59 @@
 import {useState} from 'react'
 
-const Hello = (props) => {
-	const bornYear = ()=>{
-		const yearNow = new Date().getFullYear();
-		return yearNow - props.age;
-	}
+const Hello = ({name,age}) => {
+    const bornYear = () => {
+        const yearNow = new Date().getFullYear();
+        return yearNow - age;
+    }
 
-	return (
-		      <div>
-		        <p>
-		          Hello {props.name}, you are {props.age} years old
-		        </p>
-			<p>So you were probably born in {bornYear()}</p>
-		      </div>
-		    )
+    return (
+        <div>
+            <p>
+                Hello {name}, you are {age} years old
+            </p>
+            <p>So you were probably born in {bornYear()}</p>
+        </div>
+    )
 }
 
-const Footer = () => {
-	  return (
-		      <div>
-		        greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
-		      </div>
-		    )
-}
+const Footer = () => (
+    <div>
+        greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
+    </div>
+)
 
-const Counter = ({num})=>{
-	return (
-		<div>
-			计数器:{num}
-		</div>
-	)
-}
+const Counter = ({num}) => (
+    <div>
+        计数器:{num}
+    </div>
+)
+
+
+const Button = ({onClick, text}) => (
+    <button onClick={onClick}>
+        {text}
+    </button>
+)
 
 const App = () => {
-	  const name = 'Peter'
-	  const age = 100
-	  let [counter,setCounter] = useState(0)
-	  const handleClick = () => {
-	      console.log('clicked')
-		  setCounter(counter+1)
-	  }
-
-	return (
-		    <>
-		      <h1>Greetings</h1>
-		      <Hello name="Maya" age={26 + 10} />
-		      <Hello name={name} age={age} />
-			  <Counter num={counter} />
-			  <button onClick={handleClick}>
-					点击加1
-			  </button>
-		      <Footer />
-		    </>
-		  )
+    const name = 'Peter'
+    const age = 100
+    let [counter, setCounter] = useState(0)
+    const increaseByOne = () => setCounter(counter + 1)
+    const setToZero = () => setCounter(0)
+    const decreaseByOne = () => setCounter(counter - 1)
+    return (
+        <>
+            <h1>Greetings</h1>
+            <Hello name="Maya" age={26 + 10}/>
+            <Hello name={name} age={age}/>
+            <Counter num={counter}/>
+            <Button onClick={increaseByOne} text={'加一'}/>
+            <Button onClick={setToZero} text={'归零'}/>
+            <Button onClick={decreaseByOne} text={'减一'}/>
+            <Footer/>
+        </>
+    )
 }
 
 export default App;
