654f0f50a0c646df3d5418cba595c75bac98ec0e
ppwqdxlte <rexinwangmin@foxmail.com>
2022-06-26 23:24:30 +0800

    渲染集合：约定li最好包含key属性，否则控制台爆红不好看

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,13 +1,17 @@
 const App = (props) => {
-  const { notes } = props
-  return (
-      <>
-        <h1>Notes</h1>
-        <ul>
-            {notes.map(note => <li>{note.date}<br />{note.content}</li>)}
-        </ul>
-      </>
-  )
+    const {notes} = props
+    return (
+        <>
+            <h1>Notes</h1>
+            <ul>
+                {notes.map(note =>
+                    <li key={note.id}>
+                        {note.date}<br/>{note.content}
+                    </li>
+                )}
+            </ul>
+        </>
+    )
 }
 
 export default App;
