b578bb3c507cf7187c2f505f6f2be1699e244235
ppwqdxlte <rexinwangmin@foxmail.com>
2022-06-26 23:13:53 +0800

    渲染集合：笨方法罗列出li

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,26 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-const App = () => {
-  console.log('IDEA添加了一个React Snippets的组件，定制快捷代码非常好用')
+const App = (props) => {
+  const { notes } = props
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <>
+        <h1>Notes</h1>
+        <ul>
+          <li>{notes[0].content}</li>
+          <li>{notes[1].content}</li>
+          <li>{notes[2].content}</li>
+        </ul>
+      </>
+  )
 }
 
 export default App;
