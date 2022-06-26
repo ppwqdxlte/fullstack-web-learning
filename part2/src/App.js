b86fb8668a22b5b3fccdbd4f6791eda5a2f2ba10
ppwqdxlte <rexinwangmin@foxmail.com>
2022-06-26 23:21:09 +0800

    渲染集合：map函数迭代处理数组对象

--- part2/src/App.js
+++ part2/src/App.js
@@ -4,9 +4,7 @@ const App = (props) => {
       <>
         <h1>Notes</h1>
         <ul>
-          <li>{notes[0].content}</li>
-          <li>{notes[1].content}</li>
-          <li>{notes[2].content}</li>
+            {notes.map(note => <li>{note.date}<br />{note.content}</li>)}
         </ul>
       </>
   )
