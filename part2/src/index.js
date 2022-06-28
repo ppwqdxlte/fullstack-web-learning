322fbb892246eed27fa6c13e9e3c8e6d8e7f4955
ppwqdxlte <rexinwangmin@foxmail.com>
2022-06-28 20:34:46 +0800

    axios/promise方式:从json-server获取数据

--- part2/src/index.js
+++ part2/src/index.js
@@ -3,28 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-/*XHR方式获取json服务器的数据*/
-const xhttp = new XMLHttpRequest()
-xhttp.onreadystatechange = () => {
-    //【this问题】this在这里undefined，只能用xhttp引用readyState和status
-    console.log(this)
-    if (xhttp.readyState === 4 && xhttp.status === 200){
-        const data = JSON.parse(xhttp.responseText)
-        // andle the response that is saved in variable data
-        root.render(
-            <React.StrictMode>
-                <App notes={data}/>
-            </React.StrictMode>
-        )
-    }
-}
-xhttp.open('GET','http://localhost:3001/notes',true)
-xhttp.send()
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const root = ReactDOM.createRoot(document.getElementById('root'))
+/*Axios和Promise 方式获取json服务器的数据*/
+const promise = axios.get('http://localhost:3001/notes')
+// console.log(promise) //fulfilled( resolved)
+promise.then(response => {
+    console.log(response)
+    root.render(
+        <React.StrictMode>
+            <App notes={response.data}/>
+        </React.StrictMode>
+    )
+})
+// const promise2 = axios.get('http://localhost:3001/foobar')
+// console.log(promise2) //rejected
 
 reportWebVitals(console.log);
