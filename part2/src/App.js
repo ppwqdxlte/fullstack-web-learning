097641551fdd097e3e88880bc8973a9aaa788e2b
ppwqdxlte <rexinwangmin@foxmail.com>
2022-06-28 13:54:48 +0800

    表单：用状态newNote保存组件input的变化，用状态notes内存中临时记录笔记，钩子setNewNote处理onchange事件，setNotes处理submit提交事件

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,14 +1,43 @@
 import Note from './components/Note'
+import {useState} from "react";
 
-const App = ({notes}) =>
-    <>
-        <h1>Notes</h1>
-        <ul>
-            {/*不推荐用(..., i)=>...带索引的方式，有大大问题*/}
-            {notes.map(note =>
-                <Note key={note.id} note={note} />
-            )}
-        </ul>
-    </>
+const App = (props) => {
+    const [notes,setNotes] = useState(props.notes)
+    const [newNote,setNewNote] = useState('a new note...')
+
+    const addNote = (event) => {
+        event.preventDefault()
+        //console.log('button clicked',event.target)
+        setNotes(notes.concat({
+            id:notes.length + 1,
+            content:newNote,
+            date:new Date().toISOString(),
+            important:Math.random() < 0.5
+        }))
+        setNewNote('')
+    }
+
+    const handleNoteChange = (event) => {
+        //console.log(event.target.value)
+        setNewNote(event.target.value)
+    }
+    
+    return (
+        <>
+            <h1>Notes</h1>
+            <ul>
+                {/*不推荐用(..., i)=>...带索引的方式，有大大问题*/}
+                {notes.map(note =>
+                    <Note key={note.id} note={note}/>
+                )}
+            </ul>
+            <form onSubmit={addNote}>
+                <input value={newNote} onChange={handleNoteChange}/>
+                <button type="submit">保存</button>
+            </form>
+        </>
+    )
+
+}
 
 export default App;
