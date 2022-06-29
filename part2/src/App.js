baa341911620a0352af2a8a0ef80465bc76d4e82
ppwqdxlte <rexinwangmin@foxmail.com>
2022-06-29 11:55:02 +0800

    useEffect方式:拉取数据的操作从index.js转移到App.js，之前用axios获取的数据直接传递给app组件的参数，用app就不能裸露调用axios，否则因为每次调用setNotes导致无限刷新app组件，状态钩子不可以在callback中定义，但可以在callback中引用，组件有state的话，若想用axios，必须在axios的外面套一层useEffect【效果钩子】防止无限刷新。

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,47 +1,54 @@
+import {useState, useEffect} from "react";
+import axios from 'axios'
 import Note from './components/Note'
-import {useState} from "react";
 
-const App = (props) => {
-    const [notes,setNotes] = useState(props.notes)
-    const [newNote,setNewNote] = useState('a new note...')
-    const [showAll,setShowAll] = useState(true)
-
+const App = () => {
+    const [notes, setNotes] = useState([])
+    const [newNote, setNewNote] = useState('add a new note...')
+    const [showAll, setShowAll] = useState(true)
     const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
+
+    const handleNoteChange = (event) => {
+        setNewNote(event.target.value)
+    }
 
     const addNote = (event) => {
         event.preventDefault()
-        //console.log('button clicked',event.target)
         setNotes(notes.concat({
-            id:notes.length + 1,
-            content:newNote,
-            date:new Date().toISOString(),
-            important:Math.random() < 0.5
+            id: notes.length + 1,
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random() < 0.5
         }))
         setNewNote('')
     }
 
-    const handleNoteChange = (event) => {
-        //console.log(event.target.value)
-        setNewNote(event.target.value)
-    }
-    
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/notes')
+            .then(response => {
+                console.log('promise fulfilled')
+                setNotes(response.data)
+            })
+    }, [])
+    console.log('render', notesToShow.length, 'notes')
+
     return (
         <>
             <h1>Notes</h1>
-            <button onClick={()=>setShowAll(!showAll)}>show {showAll ? 'all' : 'important'}
-            </button>
-            <ul>
-                {notesToShow.map(note =>
-                    <Note key={note.id} note={note}/>
-                )}
-            </ul>
-            <form onSubmit={addNote}>
-                <input value={newNote} onChange={handleNoteChange}/>
-                <button type="submit">保存</button>
+            <button onClick={() => setShowAll(!showAll)}>show {showAll ? 'all' : 'important'}</button>
+            {notesToShow.map(note =>
+                <ul key={note.id}>
+                    <Note note={note}/>
+                </ul>
+            )}
+            <form  onSubmit={addNote}>
+                <input onChange={handleNoteChange} value={newNote}/>
+                <button type='submit'>添加</button>
             </form>
         </>
     )
-
 }
 
 export default App;
--- part2/src/index.js
+++ part2/src/index.js
@@ -3,21 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import axios from 'axios';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-/*Axios和Promise 方式获取json服务器的数据*/
-const promise = axios.get('http://localhost:3001/notes')
-// console.log(promise) //fulfilled( resolved)
-promise.then(response => {
-    console.log(response)
-    root.render(
+ReactDOM
+    .createRoot(document.getElementById('root'))
+    .render(
         <React.StrictMode>
-            <App notes={response.data}/>
+            <App />
         </React.StrictMode>
     )
-})
-// const promise2 = axios.get('http://localhost:3001/foobar')
-// console.log(promise2) //rejected
-
 reportWebVitals(console.log);
