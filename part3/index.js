2108c445bdc916b5c7d66498c0aa5e57228afdbc
laowang <rexinwangmin@foxmail.com>
2022-07-01 20:52:37 +0800

    RESTfulAPI：POST方法实现一个【添加资源】的路径，json数据格式

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+//激活express的 json-parser 来处理HTTP POST、PUT等请求，方便在request body中以JSON格式传输数据
+app.use(express.json())
 
 let notes = [
     {
@@ -33,17 +35,55 @@ app.get('/api/notes', (request, response) => {
 app.get('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id) //url参数为string类型
     const note = notes.find(note => note.id === id)
-    if (note){
+    if (note) {
         response.json(note)
-    }else {
+    } else {
         response.status(404).end('<h1>ERROR!Not found!!!</h1>')
     }
 })
 
-app.delete('/api/notes/:id',(request,response)=>{
+app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     notes = notes.filter(note => note.id !== id)
     response.status(204).end() //204表示无内容
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    /* 这样写肯定是没结果的
+    const note = request.body
+    console.log(note,request.headers) //{} {...请求头信息略}
+    response.json(note) //{}
+    */
+    /*const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+    const note = request.body
+    note.id = maxId + 1
+    notes = notes.concat(note)
+    response.json(note)*/
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+
+    response.json(note)
 })
 
 const PORT = 3002
