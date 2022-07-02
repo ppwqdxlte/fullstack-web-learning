994acce29798bc969c6141d1b5218869477710f4
laowang <rexinwangmin@foxmail.com>
2022-07-02 21:00:12 +0800

    part3课后作业：通讯录后端应用，目前实现增删查功能，返回json响应数据

---
+++ part3-phonebook/index.js
@@ -0,0 +1,76 @@
+const express = require("express")
+const app = express()
+app.use(express.json())
+
+let persons = [
+    {"id": 1, "name": "Arto Hellas", "number": "040-123456"},
+    {"id": 2, "name": "Ada Lovelace", "number": "39-44-5323523"},
+    {"id": 3, "name": "Dan Abramov", "number": "12-43-234345"},
+    {"id": 4, "name": "Mary Poppendieck", "number": "39-23-6423122"}
+]
+app.get('/', (req, res) => {
+    res.send(`<h1>Phonebook</h1>`)
+})
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+app.get('/info', (req, res) => {
+    const len = persons.length
+    const ts = new Date().toLocaleString()
+    res.send(`<p>Phonebook has info for ${len} people</p>
+                    <p>${ts}</p>`)
+})
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+    if (!person) {
+        res.status(404).send(`<h3>Person of id ${id} is not found~</h3>`).end()
+    } else {
+        res.json(person)
+    }
+})
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+    if (!person) {
+        res.status(204).end()
+    } else {
+        persons = persons.filter(p => p.id !== id)
+        res.json(person).end()
+    }
+})
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(p => p.id))
+        : 0
+    return maxId + 1
+}
+const generatePhoneNumber = () => {
+    const n2 = Math.floor(Math.random() * 100).toString().padStart(2, '0')
+    const n7 = Math.floor(Math.random() * 10000000).toString().padStart(7, '0')
+    return n2.concat('-').concat(n2).concat('-').concat(n7)
+}
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    if (!body) {
+        res.status(204).end(() => console.error('没东西添加什么啊？！'))
+        return
+    }
+    const name = body.name
+    if (!name) {
+        res.status(400).send({error: 'should have a name!'}).end(() => console.error('name为空！！'))
+        return
+    }
+    if (persons.find(p => p.name === name)) {
+        res.status(400).send({error: 'name must be unique'}).end(() => console.log('电话簿已经重名了'))
+        return
+    }
+    const id = generateId()
+    const number = generatePhoneNumber()
+    const newPerson = {id, name, number}
+    res.json(newPerson)
+    persons = persons.concat(newPerson)
+})
+
+const PORT = 3003
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
